01d2ff03318346947d75d4423ae888dbe58701f9
AcodonicKing <acodking4@gmail.com>
2025-04-10 15:39:37 +0300

    v0.1.0 Update

--- media/textures/components/resize.py
+++ media/textures/components/resize.py
@@ -1,8 +1,9 @@
 from PIL import Image
 import os
-fold = "./indicator/universal/"
+fold = "./hybrid/parallel_path_selector/"
 coef = 10
 for i in os.listdir(fold):
+    print(i)
     if os.path.isfile(fold+i):
         img = Image.open(fold+i)
         img = img.resize(tuple([j * coef for j in img.size]), resample = Image.NEAREST)
